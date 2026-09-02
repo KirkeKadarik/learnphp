d9e78d0a76999d4125cd697a002485015f4eee57
Kirke Kadarik <Kirke.Kadarik@tptlive.ee>
2026-09-02 13:21:04 +0300

    add variable ex.

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-echo "hello world!";
-
+echo "hello world!"; // lühike komm
+/*
+*pikk kommentaar 
+*/
+$name = "Nimi";
+$age = 22;
+echo $age; 
+$age = $age * 2 * 3 * 4;
+echo $age;
+echo 2 + 3;
 ?>
