60e90446a56b0da82cd3356ea98ebd76fe43aa46
Kirke Kadarik <Kirke.Kadarik@tptlive.ee>
2026-09-04 13:18:21 +0300

    add string ex.

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
-$test = 10;
-$test = 10 + 2;
-$test = 10 - 2;
-$test = 10 * 2;
-$test = 10 / 2;
-$test = $test - 1;
-$test -= 1; // teeb sama mida 7 rida 
-$test += 1;
-$test /= 1;
-$test = 10 % 3; //jäägiga jagamine
-$test++;
-$test--;
-$test = (10 + (2 - 3) * 4) /2;
-$test %= 2;
-$test = 2 ** 4;
-$test **= 3;
-var_dump($test);
+$test = 'hello';
+$test = 'hello' . 'world';
+$test .= '!';
+$name = 'kirke';
+$age = 22;
+$test = $name . 'is' . $age . 'years old';
+$test = "$name is $age years old";
+$test = "hello\n\nworld";
+$letter = 'a';
+$letter++;
+var_dump($letter);
