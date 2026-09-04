a59d1f0253fce9b6b9aaa71b1a47442aea4976b3
Kirke Kadarik <Kirke.Kadarik@tptlive.ee>
2026-09-04 13:50:55 +0300

    add boolean math ex

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
-$test = 'hello';
-$test = 'hello' . 'world';
-$test .= '!';
-$name = 'kirke';
-$age = 22;
-$test = $name . 'is' . $age . 'years old';
-$test = "$name is $age years old";
-$test = "hello\n\nworld";
-$letter = 'a';
-$letter++;
-var_dump($letter);
+$test = true;
+$test = false;
+$test = true && false;
+$test = true || false;
+$test = !false;
+$test = 10 > 5;
+$test = 10 < 5;
+$test = 10 < 10;
+$test = 10  == 10;
+$test = 10  === 10;
+$test = 10  >= 10;
+$test = 10  <= 10;
+$test = 10  != 5;
+$test = 10  !== 5;
+$test = 10  == '10'; // kas nr 10 ja tekst 10 on sama väärtusega
+$test = 10  === '10';
+$test = true && false || (true && !true);
+var_dump($test);
