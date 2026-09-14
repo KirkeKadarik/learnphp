949e6c10bc91bdfcef145799cec9bc310c700869
Kirke Kadarik <Kirke.Kadarik@tptlive.ee>
2026-09-14 15:19:50 +0300

    Add U.S page and posts and title

--- us.php
+++ us.php
@@ -1,27 +1,27 @@
 <?php 
-$title = 'World';
+$title = 'U.S.';
 $posts = [
   [
-    'title' => 'some world title 1',
-   'content' => 'some world content 1',
+    'title' => 'some U.S world title 1',
+   'content' => 'some U.S world content 1',
     'date' => 'January 1, 2021',
      'author' => 'Pets',
      ],
 
-  ['title' => 'some world title 2',
-   'content' => 'some world content 2',
+  ['title' => 'some U.S world title 2',
+   'content' => 'some U.S world content 2',
     'date' => 'January 2, 2021',
      'author' => 'Dogs',
       ],
 
-  ['title' => 'some world title 3',
-   'content' => 'some world content 3',
+  ['title' => 'some U.S world title 3',
+   'content' => 'some U.S world content 3',
     'date' => 'January 3, 2021',
      'author' => 'Cats',
      ],
      
-['title' => 'some world title 4',
-   'content' => 'some world content 4',
+['title' => 'some U.S world title 4',
+   'content' => 'some U.S world content 4',
     'date' => 'January 4, 2021',
      'author' => 'Seals',
      ],
@@ -29,9 +29,10 @@ $posts = [
 ]
 
 ?>
-    
-
 <?php include __DIR__. '/partials/header.php';?>
+  
+   
+
     <main class="container">
  <?php include __DIR__. '/partials/hero.php';?>
     <?php include __DIR__. '/partials/featured.php';?>
